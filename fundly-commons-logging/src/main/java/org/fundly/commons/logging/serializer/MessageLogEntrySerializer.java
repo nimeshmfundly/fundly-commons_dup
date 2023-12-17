4956dfda01ecd931baf21d0784b5d2d7992983f6
nimesh <nimesh.m@fundly.ai>
2023-12-17 14:47:26 +0530

    Log entry serializer enhanced with jsonGenerator

--- fundly-commons-logging/src/main/java/org/fundly/commons/logging/serializer/MessageLogEntrySerializer.java
+++ fundly-commons-logging/src/main/java/org/fundly/commons/logging/serializer/MessageLogEntrySerializer.java
@@ -2,7 +2,6 @@ package org.fundly.commons.logging.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import org.fundly.commons.logging.model.MessageLogEntry;
@@ -12,6 +11,9 @@ public class MessageLogEntrySerializer extends JsonSerializer<MessageLogEntry> {
   public void serialize(MessageLogEntry messageLogEntry,
                         JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
       throws IOException {
-    System.out.println("checking json generator" + jsonGenerator.getCurrentValue().toString());
+    jsonGenerator.writeStartObject();
+    System.out.println("checking json generator" + jsonGenerator);
+    jsonGenerator.writeStringField("message", messageLogEntry.getMessage());
+    jsonGenerator.writeEndObject();
   }
 }
